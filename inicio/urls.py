9965c972802cb1495b0d6499b5ff18cd8f48f284
Gabriel Samacoits <cesargabrielsamacoits@gmail.com>
2026-05-11 17:17:55 -0300

    2026-05-11 Prueba 3

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from inicio.views import inicio , bienvenida , fecha 
+from inicio.views import inicio , bienvenida , fecha , template1
 
 app_name = 'inicio'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', inicio, name='inicio'),
     path('bienvenida/', bienvenida, name='bienvenida'),
     path('fecha/', fecha, name='fecha'),
+    path('template1/', template1, name='template1'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from django.template import Template, Context
 
 # Create your views here.
 def inicio(request):
@@ -13,3 +14,18 @@ def fecha(request):
     import datetime
     fecha_actual = datetime.datetime.now()
     return HttpResponse(f"La fecha y hora actual es: {fecha_actual}")
+
+def template1(request):
+    
+    nom = "Cesar"
+    apellido = "Samacoits"
+    
+    diccionario = {"nombre": nom, "apellido": apellido}
+    
+    miHTML = open("C:\\Users\\cesar\\Downloads\\Python\\djangogabyinventario\\template1\\template1.html")
+    
+    plantilla = Template(miHTML.read())
+    miHTML.close()
+    contexto = Context(diccionario)
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
