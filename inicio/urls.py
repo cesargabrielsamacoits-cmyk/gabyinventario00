1b75541724b2c122f9436e81f02a279f54e60eb7
Gabriel Samacoits <cesargabrielsamacoits@gmail.com>
2026-05-11 16:34:06 -0300

    2026-05-11 prueba 2

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from inicio.views import inicio
+from inicio.views import inicio , bienvenida , fecha 
 
 app_name = 'inicio'
 
 urlpatterns = [
     path('', inicio, name='inicio'),
+    path('bienvenida/', bienvenida, name='bienvenida'),
+    path('fecha/', fecha, name='fecha'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return HttpResponse("Hola Bubucita")
+
+
+def bienvenida(request):
+    return HttpResponse("Bienvenida a mi sitio web")
+
+def fecha(request):
+    import datetime
+    fecha_actual = datetime.datetime.now()
+    return HttpResponse(f"La fecha y hora actual es: {fecha_actual}")
